f40c56cd2e74b88935ce67e9d6484c0fc994fbee
DESKTOP-GOL9RVD\SALPTP <naqeebullah656@gmail.com>
2022-02-04 15:29:36 +0500

    Simple Laravel API

---
+++ app/Http/Controllers/DumyAPIController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class DumyAPIController extends Controller
+{
+    public function getData()
+    {
+        return ["name" => " Code With Naqeeb "];
+    }
+}
+
+
